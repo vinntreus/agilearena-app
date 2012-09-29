5ef3b9c51a727e76f496189676d5d4d39e837664
Daniel Vinntreus <gudski@gmail.com>
2012-09-29 21:53:09 +0200

    refactor: backlog_item_create_handler to use event_handler
    Results in reuse of code.

--- features/backlog-item/backlog_item_create_handler.js
+++ features/backlog-item/backlog_item_create_handler.js
@@ -1,64 +1,37 @@
-var db = require(NODE_APPDIR + '/db'),
-    backlogReadstore = require(NODE_APPDIR + '/backlog_readstore'),
-    es = require(NODE_APPDIR + '/event_store'),
-    Backlog = require(NODE_APPDIR + '/domain/backlog');
+var backlogReadstore = require(NODE_APPDIR + '/backlog_readstore'),
+    Backlog = require(NODE_APPDIR + '/domain/backlog'),
+    db = require(NODE_APPDIR + '/db.js'),
+    eh = require(NODE_APPDIR + '/event_handler');
 
-var createBacklogItemHandler = (function() {
-
-/* @callback :function (error, backlogItemId) */
-  var createBacklogItem = function(options, callback) {    
-    var backlogId = options.backlogId;
-    var createdBy = options.createdBy;
-    var backlogItem = options.backlogItem;
-              
-    db.setId(backlogItem);
-    db.setCreated(backlogItem, createdBy);
-
-    es.getAggregateRoot(backlogId, Backlog, null, function(backlog, events){    
-      var error = backlog.addItem(backlogItem);
-      if(error != null) {
-        return callback(error);
-      }        
-      var createBacklogItemEvent = buildEvent(backlogItem);        
-      storeEvent(backlogId, createBacklogItemEvent, createdBy, callback);      
-    });
-  };
-
-  var buildEvent = function(backlogItem) {
-    return {
-      type : "CreatedBacklogItemEvent",
-      data : backlogItem,
-      run : function(backlog){
-        backlog.items.push(this.data);
-      }
-    };
-  };
-
-  var storeEvent = function(backlogId, createEvent, createdBy, callback) {
-    es.addEvents(backlogId, [createEvent], createdBy, function(error, events) {
-      if(error != null) {
-        console.log("createBacklogItem::storeEvent::Error::", error);
-        return callback("Could not store backlogitem");
-      }      
-      updateReadModel(backlogId, events[0].data, callback);
-    });
-  };
-
-  var updateReadModel = function(backlogId, backlogItemData, callback) {
-    backlogReadstore.addBacklogItem(backlogId, backlogItemData, function(error) {
-      var errorMessage;
-      if(error != null) {
-        console.log("createBacklogItem::updateReadModel::Error::", error);
-        errorMessage = "Could not save backlogitem";
-      }      
-      callback(errorMessage, backlogItemData._id);
-    });
-  };
-
+var createdBacklogItemEvent = function(backlogItem) {
   return {
-    createBacklogItem : createBacklogItem
+    type : "CreatedBacklogItemEvent",
+    data : backlogItem,
+    run : function(backlog){
+      backlog.items.push(this.data);
+    }
   };
+};
 
-}());
+var createBacklogItem = function(options, callback) {    
+  var createdBy = options.createdBy;
+  var backlogItem = options.backlogItem;
+            
+  db.setId(backlogItem);
+  db.setCreated(backlogItem, createdBy);
+  var e = createdBacklogItemEvent(backlogItem);
 
-module.exports = createBacklogItemHandler;
+  var o = {
+    arId : options.backlogId,
+    arType : Backlog,
+    events : [e],
+    createdBy : createdBy,
+    runCommand : function(root) {
+      return root.addItem(e.data);
+    },
+    onUpdateReadModel : backlogReadstore.addBacklogItem
+  };
+  eh.process(o, callback); 
+};
+
+module.exports = { createBacklogItem : createBacklogItem };
