663df8818e7574219f04b0efc09252750fe8d547
Daniel Vinntreus <gudski@gmail.com>
2012-10-22 20:51:09 +0200

    Toggle toolbar.

--- public/aa.backlogitems.js
+++ public/aa.backlogitems.js
@@ -3,11 +3,24 @@ var aa = aa || {};
 aa.backlogitems = (function(){
   var _container;
 
+  var toggleToolbar = function(){
+    var showToolbar = $(".selected", _container).length > 0;
+    var toolbar = $(".toolbar-container");
+    if(showToolbar){
+      toolbar.show();
+    }
+    else{
+      toolbar.hide();
+    }
+  }
+
   var initSelectedEvents = function(){
+    
     _container.on("click", function(e){
       var target = $(e.target);      
       if(target.is("input[type='checkbox']")){        
         target.parents("tr").toggleClass("selected");
+        toggleToolbar();
       }
     });
   };
@@ -24,6 +37,7 @@ aa.backlogitems = (function(){
     init : function(){
       _container = $(".backlog-items");
       initSelectedEvents();
+
     },
     setSortOrder : setSortOrder
   };
